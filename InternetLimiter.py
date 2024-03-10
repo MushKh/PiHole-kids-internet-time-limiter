c35f6f85964d5c62ebb8e62c4b04129786c3f056
MushKh <khmushegh@gmail.com>
2024-03-10 18:40:12 +0400

    Update InternetLimiter.py

--- InternetLimiter.py
+++ InternetLimiter.py
@@ -61,7 +61,8 @@ if __name__ == "__main__":
 
     # Set the Pi-hole API URL and authentication token
     #api_url = 'http://192.168.2.10/admin/index.php?login'
-    auth_token = '&auth=3f4fa74468f336df5c4cf1d343d160f8948375732f82ea1a057138ae7d35055c'
+    auth_token = '&auth=xxx'
+    
     api_url = 'http://192.168.2.10/admin/api.php?status'+auth_token
     #auth_token = 'raspberry'
 
